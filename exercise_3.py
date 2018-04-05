5fc8de7960c4688bdb787c48bf8641a36e8ebffd
dawidbudzynski <budzynski.dawid@gmail.com>
2018-04-05 20:15:14 +0200

    exercise 3 done

---
+++ exercise_3.py
@@ -0,0 +1,21 @@
+# Take a list, say for example this one:
+#
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+#
+# Extras:
+#
+# Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this
+# list in it and print out this new list.
+# Write this in one line of Python.
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller
+# than that number given by the user.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+new_list = []
+user_number = int(input('Enter a number'))
+for number in a:
+    if number < user_number:
+        new_list.append(number)
+
+print(new_list)
